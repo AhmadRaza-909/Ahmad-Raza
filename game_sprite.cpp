00297d4cafe3fe753c0f6e09f76a1176d86a66fd
Ahmad Raza <ahmad.raza2201003@gmail.com>
2024-09-19 19:13:51 +0500

    Add files via upload

---
+++ game_sprite.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using nameespace std;
+main()
+{
+	system("color F8");
+
+cout<<"  (_O_    "<<endl;
+cout<<"    W )   "<<endl;
+cout<<"   /"\__  "<<endl;
+cout<<" _|     ' "<<endl;
+}
+                                           
